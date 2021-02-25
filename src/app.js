e1b69b1eb3cf5c5fdf96eed49638f8eeb69894ec
gjmatias <gmatiasm@everis.com>
2021-02-25 07:23:11 -0500

    archivo base version 4

--- src/app.js
+++ src/app.js
@@ -6,5 +6,11 @@ const app = express()
 //Middlewares
 
 //Routes
+app.get( '/', (req, res) => {
+    res.send("HOLA MUNDO")
+})
 
 //Starting the server
+app.listen( 3000, () => {
+    console.log("El servidor se levanto en el puerto 3000");
+})
