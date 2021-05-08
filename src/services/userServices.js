5ab029fd88916d7d1a9f9d6f6e157dcfeed3d7f4
Giancarlo <matiasmauriciogiancarlo@gmail.com>
2021-05-08 17:02:56 -0500

    updated

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,40 +1,48 @@
-const database = require("../data/userDatabase")
+const fs = require('fs');
+// const database = require("../data/userDatabase")
+
+
+
+const database_users = fs.readFileSync('src/data/users.json', 'utf-8'); //Para extraer los datos del archivo.json
+const usersDatabase = JSON.parse(database_users);
 
 const findUsers = () => {
-    const response = database.usersDatabase;
+    const response = usersDatabase;
     return response;
 }
 const findUsersById = (req) => {
     const { id_user } = req.params;
-    let data = database.usersDatabase;
+    let data = usersDatabase;
     const user = data.filter( usuario => usuario.id.toString() === id_user);
     const response = user.length ?  user : "no existe el usuario";
     return response;
 }
 const saveUser = (req) => {
     const { body } = req;
-    database.usersDatabase.push(body);
-    let response = database.usersDatabase
+    usersDatabase.push(body);
+    let response = usersDatabase;
+    const json_users = JSON.stringify(usersDatabase);
+    fs.writeFileSync('src/data/users.json', json_users, 'utf-8'); //Para guardar en un archivo de manera sincrona (busca la ruta desde el inicio)
     return response;
 }
 
 const editUserById = (req) => {
     const { id_user } = req.params;
     const { body } = req;
-    let data = database.usersDatabase;
+    let data = usersDatabase;
     const user = data.find( usuario => usuario.id.toString() === id_user);
     const userPosition = data.findIndex( usuario => usuario.id.toString() === id_user);
-    database.usersDatabase[userPosition] = body;
-    let response = database.usersDatabase;
+    usersDatabase[userPosition] = body;
+    let response = usersDatabase;
     return response;
 }
 
 const deleteUserById = (req) => {
     const { id_user } = req.params;
-    let data = database.usersDatabase;
+    let data = usersDatabase;
     const userPosition = data.findIndex( usuario => usuario.id.toString() === id_user);
-    database.usersDatabase.splice(userPosition, 1)
-    let response = database.usersDatabase;
+    usersDatabase.splice(userPosition, 1)
+    let response = usersDatabase;
     return response;
 }
 
