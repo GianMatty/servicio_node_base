0cb291c09a2731ef65274d82a8221ea497fb243b
GianMatty <matiasmauriciogiancarlo@gmail.com>
2023-01-16 15:48:48 -0500

    updated

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,15 @@
-node
-
 module.exports = mongoose;
 // const URI = 'mongodb://localhost/api-practica-2021'
 // mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+
+//EJEMPLO
+/*
+const mongoose = require('mongoose');
+const URI = 'mongodb://localhost/prueba0'; //Name de la BD y ruta local
+mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(db => console.log('DB esta conectado'))
+    .catch(err => console.error(err));
+
+module.exports = mongoose; 
+*/
--- src/models/user.js
+++ src/models/user.js
@@ -6,3 +6,15 @@ const userSchema = new Schema({
 })
 
 module.exports = mongoose.model("User", userSchema)
+
+
+
+//EJEMPLO
+/*
+const TareaSchema = new Schema({
+    titulo: { type: String, required: true },
+    descripcion: { type: String, required: true },
+})
+
+module.exports = mongoose.model('Tarea', TareaSchema)//creamos un modelo y su structura
+*/
